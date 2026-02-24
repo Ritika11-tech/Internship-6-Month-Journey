1da54218c3eca8c76b1ad138ddf8a65f345553c3
Ritika <ritikatina123@gmail.com>
2026-02-24 18:50:25 +0530

    added reversed array solution

---
+++ DSA/reversed_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+
+    cout<<"Enter the no. of elements : ";
+    cin>>n;
+
+   int arr[n];
+   for(int i=0;i<n;i++){
+    cin>>arr[i];
+   }
+
+   for(int i=0;i<n/2;i++){
+   int temp= arr[i];
+   arr[i]= arr[n-1-i];
+   arr[n-1-i] = temp;
+ }
+ cout<<"Reversed array is : \n";
+ for(int i=0;i<n;i++){
+    cout<<arr[i];
+ }
+ return 0;
+}
