562015d3de5bf358231442ecc2a313564b9eb1ff
Ritika <ritikatina123@gmail.com>
2026-03-17 18:12:28 +0530

    added css and js

---
+++ Development/Portfolio/script.js
@@ -0,0 +1,19 @@
+
+const projectSection = document.querySelector("#projects");
+const typingCard = document.querySelector("#typing");
+const calculatorCard = document.querySelector("#ac");
+
+window.addEventListener("scroll", () => {
+  const sectionTop = projectSection.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+
+  if(sectionTop < screenHeight - 100){
+    typingCard.classList.add("show-left");
+    calculatorCard.classList.add("show-right");
+  }
+   else {
+    
+    typingCard.classList.remove("show-left");
+    calculatorCard.classList.remove("show-right");
+  }
+});
